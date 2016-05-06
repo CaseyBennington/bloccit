1c89ec795f7ccc5fd8fea50632aaf638d3aaa887
Casey Bennington <cbenning@vt.edu>
2016-05-06 15:03:39 -0700

    Complete Assignment 32

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,6 @@ end
 posts = Post.all
 
 # Create Comments
-
 100.times do
   Comment.create!(
     post: posts.sample,
@@ -18,6 +17,16 @@ posts = Post.all
   )
 end
 
+# Create advertisements
+50.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: RandomData.random_number
+  )
+end
+
 puts 'Seed finished'
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
