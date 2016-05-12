8b708fa123ef1a3a615e84d4ef9f982ffd8be9f3
Casey Bennington <cbenning@vt.edu>
2016-05-12 03:05:08 -0700

    Complete Checkpoint 41

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ require 'random_data'
 # Create Users
 5.times do
   User.create!(
-  name: RandomData.random_name,
-  email: RandomData.random_email,
-  password: RandomData.random_sentence
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
   )
 end
 users = User.all
@@ -34,6 +34,7 @@ posts = Post.all
 
 100.times do
   Comment.create!(
+    user: users.sample,
     post: posts.sample,
     body: RandomData.random_paragraph
   )
